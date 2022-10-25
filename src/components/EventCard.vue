91a93c54cd92f315827b8095644a95ce620b4499
S2 資訊技術中心 王彥婷 <yyopenfolder@gmail.com>
2022-10-25 14:16:47 +0800

    create `EventCard.vue`

---
+++ src/components/EventCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="event-card">
+    <span>@ {{ event.time }} on {{ event.date }}</span>
+    <h4>{{ event.title }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EventCard',
+  // props: {
+  //   msg: String
+  // },
+  data () {
+    return {
+      event: {
+        id: 123,
+        category: 'animal welfare',
+        title: 'Cat Adoption Day',
+        description: 'Find your new feline friend at this event.',
+        location: 'Meow Town',
+        date: 'January 28, 2022',
+        time: '12:00',
+        organizer: 'Kat Laydee'
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
