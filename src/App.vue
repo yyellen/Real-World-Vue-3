9bf92e970b9d6446c4a344a475c5e1ce8f317f8d
S2 資訊技術中心 王彥婷 <yyopenfolder@gmail.com>
2022-10-25 17:18:41 +0800

    update `AboutView` and router

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Events</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <h1>Event For Good</h1>
   <router-view/>
 </template>
 
