6a272e654da8414077ee3777789ba1fe5b3f80ac
S2 資訊技術中心 王彥婷 <yyopenfolder@gmail.com>
2022-10-26 16:07:52 +0800

    make `EventCard` clickable with router-link

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="event-card">
-    <span>@{{ event.time }} on {{ event.date }}</span>
-    <h4>{{ event.title }}</h4>
-  </div>
+  <router-link to="/event/123">
+    <div class="event-card">
+      <span>@{{ event.time }} on {{ event.date }}</span>
+      <h4>{{ event.title }}</h4>
+    </div>
+  </router-link>
 </template>
 
 <script>
