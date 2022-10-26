435d2bfe846ad8a543066035e476a691144d62d8
S2 資訊技術中心 王彥婷 <yyopenfolder@gmail.com>
2022-10-26 15:06:14 +0800

    create `EventDetail.vue`

---
+++ src/views/EventDetail.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      event: []
+    }
+  }
+}
+</script>
