8bebd3ec9b6fd44e7843db431ede01edbd06cd48
yyellen <yyopenfolder@gmail.com>
2022-12-14 12:23:08 +0800

    add EventCreate.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventList from '../views/EventList.vue'
 import EventDetails from '../views/EventDetails.vue'
+import EventCreate from '../views/EventCreate.vue'
 import AboutView from '../views/AboutView.vue'
 
 const routes = [
@@ -14,6 +15,11 @@ const routes = [
     name: 'EventDetails',
     props: true,
     component: EventDetails
+  },
+  {
+    path: '/event/create',
+    name: 'EventCreate',
+    component: EventCreate
   },
   {
     path: '/about',
